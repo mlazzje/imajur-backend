1e75ed355e172805c16f70616611edb8a201bcce
Brad Montgomery <quentin888@gmx.fr>
2014-12-05 10:29:41 +0100

    Models except relations

--- model.js
+++ model.js
@@ -1,17 +1,55 @@
 
-var User = sequelize.define('users', {
+var User = sequelize.define('user', {
 	id: Sequelize.UUID,
-	username: {
+	pseudo: {
 		type: Sequelize.STRING,
 		unique: true
 	},
-	email: {
+	mail: {
 		type: Sequelize.STRING,
 		unique: true
 	},
-	role: {
-		type: Sequelize.ENUM,
-		values: ['user', 'moderator', 'admin']
+	password: {
+		type: Sequelize.STRING,
 	},
-	password: Sequelize.STRING,
+});
+
+var Image = sequelize.define('image', {
+	id: Sequelize.UUID,
+	//id_user
+	titre: {
+		type: Sequelize.STRING,
+	},
+	extension: {
+		type: Sequelize.STRING,
+		values: ['jpg', 'png', 'gif'],
+	},
+	date: {
+		type: Sequelize.DATE,
+	},
+});
+
+var Commentaire = sequelize.define('commentaire', {
+	id: Sequelize.UUID,
+	//id_user
+	//id_image
+	date: {
+		type: Sequelize.DATE,
+	},
+	content: {
+		type: Sequelize.STRING,
+	},
+	notifie: {
+		type: Sequelize.BOOLEAN,
+	},
+});
+
+var Vote = sequelize.define('vote', {
+	id: Sequelize.UUID,
+	//id_user
+	//id_image
+	point: {
+		type: Sequelize.INTEGER,
+		values: [1, -1],
+	},
 });
